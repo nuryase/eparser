5443e6f1a6f411cf67745fb3c48ebdbf53b26591
yacquub <97082343+bblankfield@users.noreply.github.com>
2023-12-28 00:17:26 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name="eparser",
-    packages=find_packages(include=["eparser"]),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eParser",
     version="0.1.0",
-    description="Library for EPUB handling and parsing",
     author="yasen",
+    description="Library for EPUB handling and parsing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nuryase/eparser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
